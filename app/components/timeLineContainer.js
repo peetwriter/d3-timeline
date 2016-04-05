a9e445829f0f89d8449e2c9b4733a317d4c8f07a
Petr Tarasenko <pereter92@gmail.com>
2016-04-05 23:40:53 +0300

    trial render

--- app/components/timeLineContainer.js
+++ app/components/timeLineContainer.js
@@ -1,4 +1,15 @@
 import React from 'react';
+import d3 from "d3";
+
+var render = (svg, scale, data, color) => {
+    var rects = svg.selectAll("rect").data(data);
+    rects.enter().append("rect")
+        .attr("x", scale)
+        .attr("y", 50)
+        .attr("width", 20)
+        .attr("height", 20)
+        .attr("fill", color)
+}
 
 class TimeLineContainer extends React.Component{
     constructor(props) {
@@ -6,12 +17,20 @@ class TimeLineContainer extends React.Component{
 
     }
     componentDidMount() {
-        console.log(this);
+        var scale = d3.scale.linear()
+            .domain([1, 5])
+            .range([0, 200]);
+
+        var svg = d3.select("#container").append("svg")
+            .attr("width",250)
+            .attr("height",250);
+
+        render(svg, scale, [1, 2, 3], "red");
+
     }
     render () {
         return (
-            <div>
-                <h1>Hello World</h1>
+            <div id="container">
             </div>
         )
     }
