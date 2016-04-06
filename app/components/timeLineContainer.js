2d71ba0eb8032b654078d13c061c284d0d350e4d
Petr Tarasenko <pereter92@gmail.com>
2016-04-06 18:43:48 +0300

    drar circles on domain scaled by date

--- app/components/timeLineContainer.js
+++ app/components/timeLineContainer.js
@@ -1,31 +1,39 @@
 import React from 'react';
 import d3 from "d3";
 
-var render = (svg, scale, data, color) => {
-    var rects = svg.selectAll("rect").data(data);
-    rects.enter().append("rect")
-        .attr("x", scale)
-        .attr("y", 50)
-        .attr("width", 20)
-        .attr("height", 20)
-        .attr("fill", color)
+var getDate = (d) =>  {
+    return new Date(d);
 }
 
 class TimeLineContainer extends React.Component{
     constructor(props) {
         super(props);
-
     }
     componentDidMount() {
-        var scale = d3.scale.linear()
-            .domain([1, 5])
-            .range([0, 200]);
+
+        var dateRange = d3.extent(this.props.dates, (d) => {return getDate(d)});
+
+        var scale = d3.time.scale()
+            .domain(dateRange)
+            .range([0, 500]);
 
         var svg = d3.select("#container").append("svg")
-            .attr("width",250)
+            .attr("width",500)
             .attr("height",250);
 
-        render(svg, scale, [1, 2, 3], "red");
+        function render (data) {
+            var circles = svg.selectAll("circle").data(data);
+
+            circles.enter().append("circle").attr("r", 2);
+
+            circles
+                .attr("cx", (d) => {return scale(getDate(d))} )
+                .attr("cy", 20);
+
+            circles.exit().remove();
+       }
+
+        render(this.props.dates);
 
     }
     render () {
--- app/index.js
+++ app/index.js
@@ -4,6 +4,6 @@ import ReactDOM from "react-dom";
 import TimeLineContainer from "./components/TimeLineContainer";
 
 ReactDOM.render(
-    React.createElement(TimeLineContainer),
+    <TimeLineContainer dates={["1/06/2015", "2/06/2015", "3/06/2015", "4/06/2015", "5/06/2015", "6/06/2015"]}/>,
     document.getElementById("app")
 )
