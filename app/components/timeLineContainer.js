ebc85f3249dd04eb80e7a575feefd4cfc345a6c5
Petr Tarasenko <pereter92@gmail.com>
2016-05-04 23:43:16 +0300

    resize without remove elements

--- app/components/timeLineContainer.js
+++ app/components/timeLineContainer.js
@@ -16,66 +16,74 @@ var createDataFromDates = (dates) => {
     return data;
 }
 
+function renderLineChart (data, dateRange, indexRange) {
+    var containerWidth = parseInt(d3.select("#container").style("width"));
+    var containerHeight = parseInt(d3.select("#container").style("height"));
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 20},
+        width = containerWidth - margin.left - margin.right,
+        height = containerHeight - margin.top - margin.bottom;
+
+
+    var x = d3.time.scale()
+        .range([0, width])
+        .domain(dateRange);
+
+    var y = d3.scale.linear()
+        .range([height, 0])
+        .domain(indexRange);
+
+    var svg = d3.select("#container")
+        .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+            .attr("transform",
+                  "translate(" + margin.left + "," + margin.top + ")");
+
+    var path = d3.svg.line().interpolate("monotone")
+        .x((d) => {
+            return x(d.date);
+        })
+        .y((d) => {
+            return y(d.index)
+        });
+
+    var circles = svg.selectAll("circle").data(data);
+    circles
+        .enter()
+        .append("circle")
+        .attr("r", 5)
+        .attr("cx",(d) => {
+            return x(d.date);
+        })
+        .attr("cy", (d) => {
+            return y(d.index);
+        });
+
+    svg.append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("d", path);
+
+    circles.exit().remove();
+}
+
 class TimeLineContainer extends React.Component{
-    constructor(props) {
-        super(props);
+    constructor(props, context) {
+        super(props, context);
+        var data = createDataFromDates(props.dates);
+        this.state = {
+            data: data,
+            dateRange: d3.extent(data, (d) => {return d.date}),
+            indexRange: d3.extent(data, (d) => {return d.index})
+        }
     }
+
     componentDidMount() {
-
-        var margin = {top: 20, right: 20, bottom: 30, left: 20},
-            width = 600 - margin.left - margin.right,
-            height = 400 - margin.top - margin.bottom;
-
-        var data = createDataFromDates(this.props.dates);
-        var dateRange = d3.extent(data, (d) => {return d.date});
-        var indexRange = d3.extent(this.props.dates, (d) => {return getDate(d)});
-
-        var x = d3.time.scale()
-            .range([0, width])
-            .domain(dateRange);
-
-        var y = d3.scale.linear()
-            .range([height, 0])
-            .domain([0, 5]);
-
-        var svg = d3.select("#container")
-            .append("svg")
-                .attr("width", width + margin.left + margin.right)
-                .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-                .attr("transform",
-                      "translate(" + margin.left + "," + margin.top + ")");
-
-        var  i = 0;
-        var path = d3.svg.line()
-            .x((d) => {
-                return x(d.date);
-            })
-            .y((d) => {
-                return y(d.index)
-            });
-
-        function render (data) {
-            var circles = svg.selectAll("circle").data(data);
-            circles.enter().append("circle").attr("r", 5);
-            circles
-                .attr("cx",(d) => {
-                    return x(d.date);
-                })
-                .attr("cy", (d) => {
-                    return y(d.index);
-                });
-
-            svg.append("path")
-                .datum(data)
-                .attr("class", "line")
-                .attr("d", path);
-
-            circles.exit().remove();
-       }
-
-        render(data);
-
+        var state = this.state;
+        renderLineChart(state.data, state.dateRange, state.indexRange);
+        d3.select(window).on('resize', () => {renderLineChart(state.data, state.dateRange, state.indexRange);})
     }
     render () {
         return (
--- app/index.js
+++ app/index.js
@@ -4,6 +4,6 @@ import ReactDOM from "react-dom";
 import TimeLineContainer from "./components/TimeLineContainer";
 
 ReactDOM.render(
-    <TimeLineContainer dates={["1/06/2015", "2/07/2015", "2/26/2015", "2/29/2015", "12/06/2015", "12/07/2015"]}/>,
+    <TimeLineContainer dates={["1/06/2015", "1/07/2015", "1/09/2015", "1/15/2015", "1/25/2015", "1/26/2015"]}/>,
     document.getElementById("app")
 )
